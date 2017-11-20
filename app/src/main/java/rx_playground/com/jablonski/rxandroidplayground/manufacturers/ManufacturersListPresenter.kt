d7d6ca4d834dce10dfecb0273e5d6f5b7b74b1fe
Patryk Jabłoński <jablonski.pj@gmail.com>
2017-11-20 20:02:59 +0100

    conversion of another file to kotlin

---
+++ app/src/main/java/rx_playground/com/jablonski/rxandroidplayground/manufacturers/ManufacturersListPresenter.kt
@@ -0,0 +1,51 @@
+package rx_playground.com.jablonski.rxandroidplayground.manufacturers
+
+import rx_playground.com.jablonski.rxandroidplayground.mvp.BaseViewCotract
+import rx_playground.com.jablonski.rxandroidplayground.model.Manufacturer
+
+/**
+ * Created by yabol on 06.04.2017.
+ */
+
+class ManufacturersListPresenter(private val view: ManufacturersViewContract.View) :
+        ManufacturersViewContract.Presenter,
+        BaseViewCotract.BaseProvider<Manufacturer>,
+        BaseViewCotract.BaseOnItemCLickListener<Manufacturer> {
+
+    private var repository: ManufacturersViewContract.Repository? = null
+    private var manufacturers: List<Manufacturer>? = null
+    fun setRepository(repository: ManufacturersViewContract.Repository) {
+        this.repository = repository
+    }
+
+    override fun loadElements(year: String) {
+        if (this.repository != null) {
+            this.repository!!.getManufacturers(year)
+        }
+    }
+
+    override fun displayElements(elements: List<Manufacturer>) {
+        this.manufacturers = elements
+        this.view.showView(elements)
+    }
+
+    override fun getElements(): List<Manufacturer>? {
+        return manufacturers
+    }
+
+    override fun getCount(): Int {
+        return if (this.manufacturers != null)
+            this.manufacturers!!.size
+        else
+            0
+    }
+
+
+    override fun getObject(position: Int): Manufacturer? {
+        return if (this.manufacturers != null) this.manufacturers!![position] else null
+    }
+
+    override fun performClick(item: Manufacturer) {
+        view.showListFragment(item.name, item.models)
+    }
+}
